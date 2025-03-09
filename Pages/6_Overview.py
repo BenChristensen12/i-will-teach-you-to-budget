15b2c0e6c174da9a6506e31d5cb39e84581ff115
Ben Christensen <benjc1212@gmail.com>
2025-03-09 16:52:05 -0400

    update tables more intuitively

--- Pages/6_Overview.py
+++ Pages/6_Overview.py
@@ -9,47 +9,7 @@ if "completed_all_tasks" not in st.session_state:
 
 else:
     st.title("Budget Overview")
-
-    df = pd.DataFrame({"Category": [], "Amount": []})
-    for table in ["fixed_costs", "savings_goals", "investments"]:
-        table_df = st.session_state[table]
-        summed_df = pd.DataFrame({"Category": [st.session_state.config["Headers"][table]], "Amount": [table_df.Amount.sum()]})
-        df = pd.concat([df, summed_df])
-
-    total = st.session_state.income.Amount.sum()
-
-    gf = pd.DataFrame({"Category": ["Guilt-Free"], "Amount": [total - df.Amount.sum()]})
-    df = pd.concat([df, gf])
-    df["Percent"] = (df.Amount / total)
-
-    #chart showing breakout of budget
-    labels = ["Net Income", "Fixed Costs", "Savings Goals", "Investments", "Guilt-Free"]
-    parents = ["", "Net Income", "Net Income", "Net Income", "Net Income"]    
-    values = [int(total), df.loc[df.Category == "Fixed Costs", "Amount"].values[0], df.loc[df.Category == "Savings Goals", "Amount"].values[0],df.loc[df.Category == "Investments", "Amount"].values[0], df.loc[df.Category == "Guilt-Free", "Amount"].values[0]]
-
-    savings_goals = st.session_state["savings_goals"].copy()
-    goals = savings_goals.Goal.tolist()
-    labels += goals
-    parents += ["Savings Goals" for goal in goals]
-    values += savings_goals.Amount.tolist()
-
-    investments = st.session_state["investments"].copy()
-    eaches = investments.Investment.tolist()
-    labels += eaches
-    parents += ["Investments" for each in eaches]
-    values += investments.Amount.tolist()
-
-    fixed_costs = st.session_state["fixed_costs"].copy()
-    grouped_df = fixed_costs.groupby("Category", as_index = False).Amount.sum()
-    categories = grouped_df.Category.tolist()
-    labels += categories
-    parents += ["Fixed Costs" for category in categories]
-    values += grouped_df.Amount.tolist()
-    labels += fixed_costs.Fixed_Cost.tolist()
-    parents += fixed_costs.Category.tolist()
-    values += fixed_costs.Amount.tolist()
-
-
+    labels, parents, values = st.session_state.sunburst_data
     sunburst = go.Figure(go.Sunburst(labels = labels, parents = parents, values = values, branchvalues = "total",))
     sunburst.update_layout(width = 800, height = 800, 
     sunburstcolorway=["#f4e4d4", "#c4442c", "#047c6c", "#142c2c"])
@@ -57,21 +17,10 @@ else:
     html_str = sunburst.to_html(full_html = True, include_plotlyjs = "cdn")
     st.download_button("Download Interactive Chart", data = html_str, file_name = "sunburst_chart.html", mime = "text/html")
 
-    # Display data
-    df["Percent"] = df["Percent"].apply(lambda x: f"{int(x*100)}%")
-    df["Goal"] = ["60%-", "10%+", "10%+", "20%+"]
-    df.sort_values("Percent", ascending = False, inplace = True)
-
-    st.dataframe(df, hide_index = True)      
+    st.dataframe(st.session_state.budget_data, hide_index = True)   
 
     # Update individual tables
-    for page in st.session_state.config["Pages"].keys():
-        for table in st.session_state.config["Pages"][page]["tables"].keys():
-            temp_df = st.session_state[table]
-            temp_df["Percent"] = temp_df.Amount / total
-            temp_df["Percent"] = temp_df["Percent"].apply(lambda x: f"{int(x*100)}%") 
-            temp_df.sort_values("Percent", ascending = False, inplace = True)
-            st.session_state[table] = temp_df.copy()      
+
     #donut chart showing breakdown of fixed costs
 
     #combined table for budget
--- utils/functions.py
+++ utils/functions.py
@@ -12,6 +12,63 @@ def initialize_dashboard():
     st.session_state["config"] = json.load(open(repo_dir + "/utils/config.json"))
     st.session_state["dashboard_initialized"] = True
 
+def update_percentages():
+    for page in st.session_state.config["Pages"].keys():
+        for table in st.session_state.config["Pages"][page]["tables"].keys():
+            temp_df = st.session_state[table]
+            temp_df["Percent"] = temp_df.Amount / st.session_state.net_income
+            temp_df["Percent"] = temp_df["Percent"].apply(lambda x: f"{int(x*100)}%") 
+            temp_df.sort_values("Percent", ascending = False, inplace = True)
+            st.session_state[table] = temp_df.copy()     
+
+def compile_budget():
+    df = pd.DataFrame({"Category": [], "Amount": []})
+    for table in ["fixed_costs", "savings_goals", "investments"]:
+        table_df = st.session_state[table]
+        summed_df = pd.DataFrame({"Category": [st.session_state.config["Headers"][table]], "Amount": [table_df.Amount.sum()]})
+        df = pd.concat([df, summed_df])
+
+    st.session_state["net_income"] = st.session_state.income.Amount.sum()
+
+    gf = pd.DataFrame({"Category": ["Guilt-Free"], "Amount": [st.session_state.net_income - df.Amount.sum()]})
+    df = pd.concat([df, gf])
+    df["Percent"] = (df.Amount / st.session_state.net_income)
+    # Display data
+    df["Percent"] = df["Percent"].apply(lambda x: f"{int(x*100)}%")
+    df["Goal"] = ["60%-", "10%+", "10%+", "20%+"]
+    df.sort_values("Percent", ascending = False, inplace = True)
+    st.session_state["budget_data"] = df.copy()
+       
+
+    #chart showing breakout of budget
+    labels = ["Net Income", "Fixed Costs", "Savings Goals", "Investments", "Guilt-Free"]
+    parents = ["", "Net Income", "Net Income", "Net Income", "Net Income"]    
+    values = [int(st.session_state.net_income), df.loc[df.Category == "Fixed Costs", "Amount"].values[0], df.loc[df.Category == "Savings Goals", "Amount"].values[0],df.loc[df.Category == "Investments", "Amount"].values[0], df.loc[df.Category == "Guilt-Free", "Amount"].values[0]]
+
+    savings_goals = st.session_state["savings_goals"].copy()
+    goals = savings_goals.Goal.tolist()
+    labels += goals
+    parents += ["Savings Goals" for goal in goals]
+    values += savings_goals.Amount.tolist()
+
+    investments = st.session_state["investments"].copy()
+    eaches = investments.Investment.tolist()
+    labels += eaches
+    parents += ["Investments" for each in eaches]
+    values += investments.Amount.tolist()
+
+    fixed_costs = st.session_state["fixed_costs"].copy()
+    grouped_df = fixed_costs.groupby("Category", as_index = False).Amount.sum()
+    categories = grouped_df.Category.tolist()
+    labels += categories
+    parents += ["Fixed Costs" for category in categories]
+    values += grouped_df.Amount.tolist()
+    labels += fixed_costs.Fixed_Cost.tolist()
+    parents += fixed_costs.Category.tolist()
+    values += fixed_costs.Amount.tolist()
+    st.session_state["sunburst_data"] = [labels, parents, values]
+
+
 def show_progress():
     st.header("Progress:")
     completed_tasks = [x in st.session_state for x in ["completed_Net_Worth", "completed_Income", "completed_Fixed_Costs", "completed_Savings_Goals", "completed_Investments"]]
@@ -20,16 +77,22 @@ def show_progress():
         st.checkbox(tasks[i], value = task_completion, disabled = True)
     if all(completed_tasks):
         st.session_state["completed_all_tasks"] = True
-    
+        compile_budget()
+        update_percentages()
+        st.rerun()
 
 def submit_changes():
     st.session_state["clicked"] = True
+
 
 def edit_data(page):
     if "clicked" in st.session_state:
         del st.session_state.clicked
         st.session_state[f"completed_{page}"] = True
         st.session_state.update(st.session_state.changed_tables)
+        if "completed_all_tasks" in st.session_state:
+            compile_budget()
+            update_percentages()        
 
     st.session_state["changed_tables"] = dict()
     tables = st.session_state.config["Pages"][page]["tables"].keys()
