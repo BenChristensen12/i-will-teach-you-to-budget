48e2f3b72859bd1a6a520a76fb26eee1c9ccb0d8
Ben Christensen <benjc1212@gmail.com>
2025-03-15 22:34:13 -0400

    Present networth

--- pages/1_Net_Worth.py
+++ pages/1_Net_Worth.py
@@ -8,3 +8,5 @@ page = os.path.basename(__file__).split('_', 1)[1].split('.')[0]
 st.title(page.replace("_", " "))
 
 edit_data(page)
+if f"completed_{page}" in st.session_state:
+    st.write(f"Net worth: {int(st.session_state.assets.Amount.sum() - st.session_state.debt.Amount.sum())}")
